3c368c5090dac44bedd8784113d951064f172d93
Osman <devosmansilva@gmail.com>
2022-07-02 10:28:31 -0500

    upload

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -18,21 +18,19 @@ class PostsController extends Controller
         $categories = DB::select("SELECT t.term_id,t.name, tm.meta_value FROM test_terms t 
         INNER JOIN test_termmeta tm ON t.term_id=tm.term_id WHERE tm.meta_key = 'cc_color' ");        
 
-<<<<<<< HEAD
         $posts = Post::status('publish')->orderBy('ID','DESC')->paginate(5);  
         $review = Post::taxonomy('category', 'Reviews')->first();
         $reviews = Post::taxonomy('category', 'Reviews')->paginate(3);
-=======
         $posts = Post::status('publish')->limit(10)->orderBy('ID','DESC')->get();  
         $review = Post::taxonomy('category', 'Reviews')->last();
         $reviews = Post::taxonomy('category', 'Reviews')->get();
->>>>>>> 271ec5c8bc82dc8da98f9921dc5817d569cef367
         $things = Post::taxonomy('category', 'Things to do')->get();
         $events = Post::taxonomy('category', 'Events')->get();
         $new = Post::taxonomy('category', 'News')->last();
         $news = Post::taxonomy('category', 'News')->get();
 
         // $cat = Post::taxonomy('post_tag','!=','')->get();
+        
 
         // dd($posts);        
 
