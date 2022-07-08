d244304e26f0fd150ae02cb14e8c969f7d1d6842
osman <devosmansilva@gmail.com>
2022-07-08 17:05:12 -0500

    add events index

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -38,13 +38,24 @@ class PostsController extends Controller
 
         $review = Post::taxonomy('category', 'Reviews')->latest()->first();
         $reviews = Post::taxonomy('category', 'Reviews')->latest()->limit(4)->get();
-        $things = Post::taxonomy('category', 'Things to do')->latest()->get();
-        $events = Post::taxonomy('category', 'Events')->latest()->get();
+        $things = Post::taxonomy('category', 'Things to do')->latest()->get();        
         $new = Post::taxonomy('category', 'News')->latest()->first();
-        $news = Post::taxonomy('category', 'News')->latest()->limit(4)->get();       
-        // dd($events->meta);        
+        $news = Post::taxonomy('category', 'News')->latest()->limit(4)->get(); 
+        
+        $events = DB::select("SELECT DISTINCT p.ID,p.post_title as title,p.post_name as slug,p.post_content as content,a.guid as image, pm.meta_value as start_event, pm2.meta_value as end_event, te.name as city
+                            FROM test_posts as a
+                            LEFT JOIN test_posts as p ON p.ID = a.post_parent AND a.post_type = 'attachment'
+                            INNER JOIN test_postmeta pm ON pm.post_id = a.post_parent AND pm.meta_key = '_EventStartDate' 
+                            INNER JOIN test_postmeta pm2 ON pm2.post_id = a.post_parent AND pm2.meta_key = '_EventEndDate'
+                            INNER JOIN test_term_relationships tr ON tr.object_id = p.ID
+                            INNER JOIN test_term_taxonomy tt ON tt.term_id = tr.term_taxonomy_id AND tt.taxonomy ='post_destinos'
+                            INNER JOIN test_terms te ON te.term_id = tt.term_id
+                            WHERE p.post_type = 'tribe_events' AND date(pm.meta_value) = current_date()
+                            ORDER BY pm.meta_value DESC LIMIT 1");  
+                            
+        // dd($events);        
 
-        return view('layouts.index', compact('reviews', 'review', 'things', 'events', 'news', 'new', 'destinations_data', 'tags_data'));
+        return view('layouts.index', compact('reviews', 'review', 'things', 'news', 'new', 'destinations_data', 'tags_data', 'events'));
     }
 
     public function post($destino, $category, $slug): View
