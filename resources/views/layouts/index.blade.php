7055ebf1f92e7865a654f4404b9b6e56dad613c4
ak21_3312 <alextyyps@gmail.com>
2022-08-18 16:46:25 -0500

    ancla

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -102,10 +102,10 @@
                                     @endif
                                     <div class="ttd-slider-item">
                                         <a
-                                            href="{{ route('guide_category', ["$ttd->destination_slug", "$ttd->category_slug"]) }}#directory-item-{!! $ttd->ID !!}">
+                                            href="{{ route('guide_category', ["$ttd->destination_slug", "$ttd->category_slug"]) }}">
                                             <div class="opacity-effect" style="border-radius: 1rem"></div>
                                             <img src="{{ images($ttd->image) }}" alt="{!! $ttd->post_title !!}"
-                                                class="carousel-img lazy">
+                                                class="carousel-img">
                                             <div class="container">
                                                 <div class="carousel-info" style="bottom:4px; z-index:2;">
                                                     <h4>{!! $ttd->post_title !!}</h4>
@@ -162,7 +162,7 @@
                                 </span>
                                 <a href="{{ url("$data->url") }}">
                                     <div class="opacity-effect" style="border-radius: 1rem 1rem 0 0;"></div>
-                                    <img src="{{ images($data->image) }}" class="card-img lazy">
+                                    <img src="{{ images($data->image) }}" class="card-img">
                                     <h3 class="card-title-overlay">
                                         {!! $data->title !!}
                                     </h3>
@@ -188,7 +188,7 @@
                                         <div class="row h-100">
                                             <div class="col card-head-secundario">
                                                 <a href="{{ url("$data->url") }}">
-                                                    <img src="{{ images($data->image) }}" class="card-img-secundario lazy">
+                                                    <img src="{{ images($data->image) }}" class="card-img-secundario">
                                                 </a>
                                             </div>
                                             <div class="col-6 card-body-secundario">
@@ -244,37 +244,37 @@
                         <div class="col-7 col-md-3 position-relative zoom">
                             <a href="{{ $gallery[0]->permalink }}" target="_blank">
                                 <i class="bi bi-instagram icon-gallery"></i>
-                                <img src="{{ $gallery[0]->media_url }}" class="lazy img-gallery-2" alt="" />
+                                <img src="{{ $gallery[0]->media_url }}" class="img-gallery-2" alt="" />
                             </a>
                         </div>
                         <div class="col-5 col-md-6 position-relative zoom">
                             <a href="{{ $gallery[1]->permalink }}" target="_blank">
                                 <i class="bi bi-instagram icon-gallery"></i>
-                                <img src="{{ $gallery[1]->media_url }}" class="lazy img-gallery" alt="" />
+                                <img src="{{ $gallery[1]->media_url }}" class="img-gallery" alt="" />
                             </a>
                         </div>
                         <div class="col-5 col-md-3 position-relative zoom">
                             <a href="{{ $gallery[2]->permalink }}" target="_blank">
                                 <i class="bi bi-instagram icon-gallery"></i>
-                                <img src="{{ $gallery[2]->media_url }}" class="lazy img-gallery" alt="" />
+                                <img src="{{ $gallery[2]->media_url }}" class="img-gallery" alt="" />
                             </a>
                         </div>
                         <div class="col-7 col-md-3 position-relative zoom">
                             <a href="{{ $gallery[3]->permalink }}" target="_blank">
                                 <i class="bi bi-instagram icon-gallery"></i>
-                                <img src="{{ $gallery[3]->media_url }}" class="lazy img-gallery-2" alt="" />
+                                <img src="{{ $gallery[3]->media_url }}" class="img-gallery-2" alt="" />
                             </a>
                         </div>
                         <div class="col-7 col-md-3 position-relative zoom">
                             <a href="{{ $gallery[4]->permalink }}" target="_blank">
                                 <i class="bi bi-instagram icon-gallery"></i>
-                                <img src="{{ $gallery[4]->media_url }}" class="lazy img-gallery" alt="" />
+                                <img src="{{ $gallery[4]->media_url }}" class="img-gallery" alt="" />
                             </a>
                         </div>
                         <div class="col-5 col-md-6 position-relative zoom">
                             <a href="{{ $gallery[5]->permalink }}" target="_blank">
                                 <i class="bi bi-instagram icon-gallery"></i>
-                                <img src="{{ $gallery[5]->media_url }}" class="lazy img-gallery-2" alt="" />
+                                <img src="{{ $gallery[5]->media_url }}" class="img-gallery-2" alt="" />
                             </a>
                         </div>
                     </div>
@@ -331,7 +331,7 @@
                                 </a>
                                 <a href="{{ url("$data->url") }}">
                                     <div class="opacity-effect" style="border-radius: 1rem 1rem 0 0;"></div>
-                                    <img src="{{ images($data->image) }}" class="card-img lazy">
+                                    <img src="{{ images($data->image) }}" class="card-img">
                                     <h3 class="card-title-overlay">
                                         {!! $data->title !!}
                                     </h3>
@@ -357,7 +357,7 @@
                                         <div class="row h-100">
                                             <div class="col card-head-secundario">
                                                 <a href="{{ url("$data->url") }}">
-                                                    <img src="{{ images($data->image) }}" class="card-img-secundario lazy">
+                                                    <img src="{{ images($data->image) }}" class="card-img-secundario">
                                                 </a>
                                             </div>
                                             <div class="col-6 card-body-secundario">
