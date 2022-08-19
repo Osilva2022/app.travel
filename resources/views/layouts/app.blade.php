c12141872978b26c5230baa3aead0b9423d25ed0
osman <devosmansilva@gmail.com>
2022-08-19 17:24:21 -0500

    disabled async

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -195,7 +195,7 @@
     <script src="{{ asset('OwlCarousel/dist/owl.carousel.min.js') }}" version="1"></script>
     <script src="{{ asset('js/base.min.js') }}" version="1"></script>
     <script src="{{ asset('js/carousels.min.js') }}" version="1"></script>
-    <script async type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.9/jquery.lazy.min.js"></script>
+    <script type="text/javascript" src="//cdnjs.cloudflare.com/ajax/libs/jquery.lazy/1.7.9/jquery.lazy.min.js"></script>
     <script async>
         $(document).ready(function() {  
             $(function() {
