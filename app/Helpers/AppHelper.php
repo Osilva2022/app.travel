d29125ae77abd2d2bc67674210c591dca6aa57cf
AlxK21 <alextyyps@gmail.com>
2022-08-30 16:40:28 -0500

    Adap Sizes 4

--- app/Helpers/AppHelper.php
+++ app/Helpers/AppHelper.php
@@ -23,10 +23,10 @@ function img_meta($data, $alt = false, $lazy = true)
     $metadatos = unserialize($data);
     $img_meta = [
         ($metadatos['image_meta']['title']) ? 'alt="' . $metadatos['image_meta']['title'] . '"' : 'title=""',
-        'width="425"',
-        'height="250"',
-        // 'width="' . $metadatos['width'] . '"',
-        // 'height="' . $metadatos['height'] . '"',
+        // 'width="425"',
+        // 'height="250"',
+        'width="' . $metadatos['width'] . '"',
+        'height="' . $metadatos['height'] . '"',
         'src="' . images((isset($metadatos['s3']['formats']['webp'])) ? $metadatos['s3']['formats']['webp'] : $metadatos['file']) . '"',
         ($alt) ? 'alt="' . $alt . '"' : 'alt="Alt Text"',
         ($lazy) ? 'loading="lazy"' : '',
