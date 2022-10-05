4995b69147b44eaca1f7d39cea0d9b2d2abac030
Osilva2022 <devosmansilva@gmail.com>
2022-10-05 12:27:57 -0500

    update redirect logout

--- routes/web.php
+++ routes/web.php
@@ -14,7 +14,7 @@ use Illuminate\Support\Facades\Route;
 | contains the "web" middleware group. Now create something great!
 |
 */
-Route::get('/vrpzfnjgypygxbvlh6ifjq ', function () {
+Route::get('/vrpzfnjgypygxbvlh6ifjq', function () {
     return redirect()->away('https://admin.tribune.travel/vrpzfnjgypygxbvlh6ifjq/');
 });
 //Redirect To Post
