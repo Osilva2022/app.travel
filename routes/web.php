77d74ebb2c4a26114753d4772286eb678c6b38ce
Osilva2022 <devosmansilva@gmail.com>
2022-12-09 17:37:15 -0600

    redirect route 404 semrush

--- routes/web.php
+++ routes/web.php
@@ -18,7 +18,7 @@ use Illuminate\Support\Facades\Redirect;
 
 //Redirect To Post
 Route::get('/vallarta-nayarit/{slug}/', [PostsController::class, 'postRedirect'])->name('vallartaRedirect');
-Route::get('/cancun-riviera-maya/{slug}/', [PostsController::class, 'postRedirect'])->name('cancunredirect');
+// Route::get('/cancun-riviera-maya/{slug}/', [PostsController::class, 'postRedirect'])->name('cancunredirect');
 Route::get('/columns/{slug}/', [PostsController::class, 'postRedirect'])->name('columnsRedirect');
 Route::get('/mexico/{slug}/', [PostsController::class, 'postRedirect'])->name('mexicoRedirect');
 Route::get('/world/{slug}/', [PostsController::class, 'postRedirect'])->name('worlsRedirect');
@@ -72,6 +72,69 @@ Route::get('/sin-categoria/vallarta-tribune-to-become-tribune-travel/', function
 Route::get('/los-cabos/3-spots-to-discover-los-cabos/', function () {
     return redirect()->route('postRedirect', '3-spots-to-discover-los-cabo');
 });
+Route::get('/los-cabos/how-to-get-to-cabo-san-lucas/', function () {
+    return redirect()->route('postRedirect', 'how-to-get-to-cabo-san-lucas');
+});
+Route::get('/cancun-riviera-maya', function () {
+    return redirect()->route('home', 301);
+});
+Route::get('/los-cabos/3-spots-to-discover-los-cabos/', function () {
+    return redirect()->route('home');
+});
+Route::get('/cancun-riviera-maya/origin-of-pan-de-muerto/', function () {
+    return redirect()->route('home');
+});
+Route::get('/los-cabos/beach-weddings-best-destination/', function () {
+    return redirect()->route('postRedirect', 'beach-weddings-best-destination');
+});
+Route::get('/los-cabos/beer-is-brewtiful-as-life/', function () {
+    return redirect()->route('postRedirect', 'beer-is-brewtiful-as-life');
+});
+Route::get('/los-cabos/cabos-longest-lazy-river-is-coming/', function () {
+    return redirect()->route('postRedirect', 'cabos-longest-lazy-river-is-coming');
+});
+Route::get('/los-cabos/cerro-del-vigia-cabo-san-lucas-as-you-have-never-seen-it/', function () {
+    return redirect()->route('postRedirect', 'cerro-del-vigia-cabo-san-lucas-as-you-have-never-seen-it');
+});
+Route::get('/los-cabos/dont-worry-beer-happy-in-oktoberfest/', function () {
+    return redirect()->route('postRedirect', 'dont-worry-beer-happy-in-oktoberfest');
+});
+Route::get('/los-cabos/how-to-get-to-cabo-san-lucas/', function () {
+    return redirect()->route('postRedirect', 'how-to-get-to-cabo-san-lucas');
+});
+Route::get('/los-cabos/how-to-relieve-stress-and-anxiety/', function () {
+    return redirect()->route('postRedirect', 'how-to-relieve-stress-and-anxiety');
+});
+Route::get('/los-cabos/la-casona-a-top-restaurant-cabo/', function () {
+    return redirect()->route('postRedirect', 'la-casona-a-top-restaurant-cabo');
+});
+Route::get('/los-cabos/planning-your-wellness-vacations/', function () {
+    return redirect()->route('postRedirect', 'planning-your-wellness-vacations');
+});
+Route::get('/los-cabos/swimming-at-palmilla-beach-cabo/', function () {
+    return redirect()->route('postRedirect', 'swimming-at-palmilla-beach-cabo');
+});
+Route::get('/los-cabos/tacos-of-many-and-any-thing/', function () {
+    return redirect()->route('postRedirect', 'tacos-of-many-and-any-thing');
+});
+Route::get('/los-cabos/the-cabo-san-lucas-arch/', function () {
+    return redirect()->route('postRedirect', 'the-cabo-san-lucas-arch');
+});
+Route::get('/los-cabos/travelers-and-tourists/', function () {
+    return redirect()->route('postRedirect', 'travelers-and-tourists');
+});
+Route::get('/los-cabos/whale-shark-season-in-cabo/', function () {
+    return redirect()->route('postRedirect', 'whale-shark-season-in-cabo');
+});
+Route::get('/los-cabos/what-is-the-deep-tissue-massage/', function () {
+    return redirect()->route('postRedirect', 'what-is-the-deep-tissue-massage');
+});
+Route::get('/los-cabos/why-you-should-visit-los-cabos-in-summer/', function () {
+    return redirect()->route('postRedirect', 'why-you-should-visit-los-cabos-in-summer');
+});
+
+
+
 Route::get('/posts/{slug}/', [PostsController::class, 'postRedirect'])->name('postRedirect');
 //Redirect To Post
 
