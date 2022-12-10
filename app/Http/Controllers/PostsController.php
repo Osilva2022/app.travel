f543eab1a37d076bae8cabd902634183db1d7bc6
Osilva2022 <devosmansilva@gmail.com>
2022-12-10 13:56:33 -0600

    sitemap metatitle

--- app/Http/Controllers/PostsController.php
+++ app/Http/Controllers/PostsController.php
@@ -614,7 +614,7 @@ class PostsController extends Controller
             return redirect()->route('home');
         }
         // $posts_info = DB::select("SELECT * FROM travel_posts_all WHERE user_nicename = '$author_id' ORDER BY post_date DESC;");
-        $posts_info = DB::select("SELECT 
+        $posts_info = DB::select("SELECT
                                         pd.*
                                     FROM
                                         travel_posts_destination AS pd
@@ -937,6 +937,14 @@ class PostsController extends Controller
         $destinations_data = $this->returndata('destinations');
         $categories_data = $this->returndata('categories');
         $tags_data = $this->returndata('tags');
+
+        $this->metadatos(
+            config('constants.META_TITLE'),
+            config('constants.META_DESCRIPTION'),
+            config('constants.DEFAULT_IMAGE'),
+            route('sitemap'),
+            route('sitemap')
+        );
         return view('sitemap.index', compact('destinations_data', 'categories_data', 'tags_data'));
     }
 
