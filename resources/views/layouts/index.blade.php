7720458fb35396fe882f60a0de7ae33e7f8ac687
Joelarumbe <joe.larumbe@lifeasbrand.com>
2023-04-12 11:53:48 -0600

    caliente adds

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -54,33 +54,32 @@
     @endpush
 @endif
 @push('caliente-ads')
-    <script data-cfasync="false" type="text/javascript" id="clever-core">
-        /* <![CDATA[ */
-        (function(document, window) {
-            var a, c = document.createElement("script"),
-                f = window.frameElement;
+        <script data-cfasync="false" type="text/javascript" id="clever-core">
+            /* <![CDATA[ */
+            (function (document, window) {
+                var a, c = document.createElement("script"), f = window.frameElement;
 
-            c.id = "CleverCoreLoader67990";
-            c.src = "https://scripts.cleverwebserver.com/6b283cdca4541795e05d7fef5d9d8d9f.js";
+                c.id = "CleverCoreLoader67990";
+                c.src = "https://scripts.cleverwebserver.com/6b283cdca4541795e05d7fef5d9d8d9f.js";
 
-            c.async = !0;
-            c.type = "text/javascript";
-            c.setAttribute("data-target", window.name || (f && f.getAttribute("id")));
-            c.setAttribute("data-callback", "put-your-callback-function-here");
-            c.setAttribute("data-callback-url-click", "put-your-click-macro-here");
-            c.setAttribute("data-callback-url-view", "put-your-view-macro-here");
+                c.async = !0;
+                c.type = "text/javascript";
+                c.setAttribute("data-target", window.name || (f && f.getAttribute("id")));
+                c.setAttribute("data-callback", "put-your-callback-function-here");
+                c.setAttribute("data-callback-url-click", "put-your-click-macro-here");
+                c.setAttribute("data-callback-url-view", "put-your-view-macro-here");
 
-            try {
-                a = parent.document.getElementsByTagName("script")[0] || document.getElementsByTagName("script")[0];
-            } catch (e) {
-                a = !1;
-            }
+                try {
+                    a = parent.document.getElementsByTagName("script")[0] || document.getElementsByTagName("script")[0];
+                } catch (e) {
+                    a = !1;
+                }
 
-            a || (a = document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]);
-            a.parentNode.insertBefore(c, a);
-        })(document, window);
-        /* ]]> */
-    </script>
+                a || (a = document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]);
+                a.parentNode.insertBefore(c, a);
+            })(document, window);
+            /* ]]> */
+        </script>
 @endpush
 <!-- content -->
 @section('content')
