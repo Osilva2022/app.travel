af18817ea79109f1cb6a9a6ecf442c22f205c509
AlxK21 <alextyyps@gmail.com>
2023-04-24 09:57:59 -0600

    caliente bye

--- resources/views/layouts/index.blade.php
+++ resources/views/layouts/index.blade.php
@@ -53,34 +53,6 @@
         </script>
     @endpush
 @endif
-@push('caliente-ads')
-        <script data-cfasync="false" type="text/javascript" id="clever-core">
-            /* <![CDATA[ */
-            (function (document, window) {
-                var a, c = document.createElement("script"), f = window.frameElement;
-
-                c.id = "CleverCoreLoader67990";
-                c.src = "https://scripts.cleverwebserver.com/6b283cdca4541795e05d7fef5d9d8d9f.js";
-
-                c.async = !0;
-                c.type = "text/javascript";
-                c.setAttribute("data-target", window.name || (f && f.getAttribute("id")));
-                c.setAttribute("data-callback", "put-your-callback-function-here");
-                c.setAttribute("data-callback-url-click", "put-your-click-macro-here");
-                c.setAttribute("data-callback-url-view", "put-your-view-macro-here");
-
-                try {
-                    a = parent.document.getElementsByTagName("script")[0] || document.getElementsByTagName("script")[0];
-                } catch (e) {
-                    a = !1;
-                }
-
-                a || (a = document.getElementsByTagName("head")[0] || document.getElementsByTagName("body")[0]);
-                a.parentNode.insertBefore(c, a);
-            })(document, window);
-            /* ]]> */
-        </script>
-@endpush
 <!-- content -->
 @section('content')
     <header>
@@ -252,8 +224,6 @@
                     </div>
                 </div>
             @endif
-            <div class="clever-core-ads">
-            </div>
             <div class="row mb-4">
                 <div class="col-12">
                     <hr>
