058d41e0053ee82a69394a7dc6d6f7ac538bc7ee
Joelarumbe <joe.larumbe@lifeasbrand.com>
2023-06-19 13:04:51 -0600

    viewport

--- resources/views/layouts/app.blade.php
+++ resources/views/layouts/app.blade.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta http-equiv="X-UA-Compatible" content="IE=edge">
-    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0, shrink-to-fit=no">
     <meta name="theme-color" content="#0d6efd"/>
     <meta name="csrf-token" content="{{ csrf_token() }}">
     {{-- <meta name="robots" content="index, follow, max-snippet:-1, max-image-preview:large, max-video-preview:-1" /> --}}
