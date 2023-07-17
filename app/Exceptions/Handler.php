2a8bd7ff6cbaa56c481afa586caea155f27c8458
Joelarumbe <joe.larumbe@lifeasbrand.com>
2023-07-17 17:22:23 -0600

    handler redirect to

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -3,6 +3,7 @@
 namespace App\Exceptions;
 
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
+use Illuminate\Support\Facades\Redirect;
 use Throwable;
 
 class Handler extends ExceptionHandler
@@ -34,8 +35,8 @@ class Handler extends ExceptionHandler
      */
     public function register()
     {
-        $this->reportable(function (Throwable $e) {
-            //
+        $this->renderable(function (Throwable $e, $request) {
+            return Redirect::to('/');
         });
     }
     
